e57acf37b5bbaa6614a20b65d532acc1fb8bf10d
mai <mai4ying1yong3@gmail.com>
2019-02-19 11:23:28 +0900

    test snow

--- game.cpp
+++ game.cpp
@@ -50,46 +50,46 @@ HRESULT InitGame(void)
 
 	// 壁の初期化 
 
-	//北壁の地面
-	InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, 640.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 64, 12, 80.0f, 80.0f);
-	//南壁の地面
-	InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, -640.0f - 960.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 64, 12, 80.0f, 80.0f);
-	//西壁の地面　反時計回転90度
-	InitMeshWall(D3DXVECTOR3(-640.0f, 0.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, D3DX_PI * 0.50f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 12, 80.0f, 80.0f);
-	//東壁の地面　時計回転90度
-	InitMeshWall(D3DXVECTOR3(640.0f, 0.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, D3DX_PI * 0.50f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 12, 80.0f, 80.0f);
+	////北壁の地面
+	//InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, 640.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 64, 12, 80.0f, 80.0f);
+	////南壁の地面
+	//InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, -640.0f - 960.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 64, 12, 80.0f, 80.0f);
+	////西壁の地面　反時計回転90度
+	//InitMeshWall(D3DXVECTOR3(-640.0f, 0.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, D3DX_PI * 0.50f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 12, 80.0f, 80.0f);
+	////東壁の地面　時計回転90度
+	//InitMeshWall(D3DXVECTOR3(640.0f, 0.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, D3DX_PI * 0.50f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 12, 80.0f, 80.0f);
 
-	//北壁
-	InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, 640.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
-	//北壁の屋上
-	InitMeshWall(D3DXVECTOR3(0.0f, 160.0f, 640.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f), 64, 12, 80.0f, 80.0f);
+	////北壁
+	//InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, 640.0f), D3DXVECTOR3(0.0f, 0.0f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	////北壁の屋上
+	//InitMeshWall(D3DXVECTOR3(0.0f, 160.0f, 640.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f), 64, 12, 80.0f, 80.0f);
 
-	//南壁　時計回転180度
-	InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, -640.0f), D3DXVECTOR3(0.0f, D3DX_PI, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
-	//南壁の屋上
-	InitMeshWall(D3DXVECTOR3(0.0f, 160.0f, -640.0f - 960.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f), 64, 12, 80.0f, 80.0f);
+	////南壁　時計回転180度
+	//InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, -640.0f), D3DXVECTOR3(0.0f, D3DX_PI, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	////南壁の屋上
+	//InitMeshWall(D3DXVECTOR3(0.0f, 160.0f, -640.0f - 960.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f), 64, 12, 80.0f, 80.0f);
 
-	//西壁　反時計回転90度
-	InitMeshWall(D3DXVECTOR3(-640.0f, 0.0f, 0.0f), D3DXVECTOR3(0.0f, -D3DX_PI * 0.50f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
-	//西壁の屋上　反時計回転90度
-	InitMeshWall(D3DXVECTOR3(-640.0f, 160.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, D3DX_PI * 0.50f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5), 16, 12, 80.0f, 80.0f);
+	////西壁　反時計回転90度
+	//InitMeshWall(D3DXVECTOR3(-640.0f, 0.0f, 0.0f), D3DXVECTOR3(0.0f, -D3DX_PI * 0.50f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	////西壁の屋上　反時計回転90度
+	//InitMeshWall(D3DXVECTOR3(-640.0f, 160.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, 0.0f, D3DX_PI * 0.50f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5), 16, 12, 80.0f, 80.0f);
 
-	//東壁　時計回転90度
-	InitMeshWall(D3DXVECTOR3(640.0f, 0.0f, 0.0f), D3DXVECTOR3(0.0f, D3DX_PI * 0.50f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
-	//東壁の屋上　時計回転90度
-	InitMeshWall(D3DXVECTOR3(640.0f, 160.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, D3DX_PI * 0.50f, 0.0f),
-									D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f), 16, 12, 80.0f, 80.0f);
+	////東壁　時計回転90度
+	//InitMeshWall(D3DXVECTOR3(640.0f, 0.0f, 0.0f), D3DXVECTOR3(0.0f, D3DX_PI * 0.50f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), 16, 2, 80.0f, 80.0f);
+	////東壁の屋上　時計回転90度
+	//InitMeshWall(D3DXVECTOR3(640.0f, 160.0f, 0.0f), D3DXVECTOR3(D3DX_PI * 0.50f, D3DX_PI * 0.50f, 0.0f),
+	//								D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.5f), 16, 12, 80.0f, 80.0f);
 
 	// 壁(裏側用)
 	//InitMeshWall(D3DXVECTOR3(0.0f, 0.0f, 640.0f), D3DXVECTOR3(0.0f, D3DX_PI, 0.0f),
@@ -123,7 +123,7 @@ HRESULT InitGame(void)
 
 	// タイマーの初期化
 	InitTimer();
-	ResetTimer(500);
+	ResetTimer(50);
 
 	// スコアの初期化
 	InitScore();
